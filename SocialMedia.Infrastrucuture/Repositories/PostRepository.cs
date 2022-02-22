2dfa14999e93a175791b1e44d40ee424f993e246
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-22 17:44:19 -0400

    Post API controller test

---
+++ SocialMedia.Infrastrucuture/Repositories/PostRepository.cs
@@ -0,0 +1,25 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastrucuture.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10)
+                .Select(x => new Post()
+                {
+                    PostId = x,
+                    userId = x,
+                    Description = $"Description {x}",
+                    Date = DateTime.Now,
+                    ImageUrl = $"https://misapis.com/{x}"
+                });
+            return posts;
+        }
+    }
+}
