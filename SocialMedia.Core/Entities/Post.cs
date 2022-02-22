3c9049bb704be7c32f32976d466daf0fba22c81d
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-22 17:44:19 -0400

    Post API controller test

---
+++ SocialMedia.Core/Entities/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SocialMedia.Core.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int userId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
