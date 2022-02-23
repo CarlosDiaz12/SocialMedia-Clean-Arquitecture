c28f589d9a799730798815cff46749eb8910481b
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-23 15:54:23 -0400

    using AutoMapper

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -12,23 +13,17 @@ namespace SocialMedia.Api.Controllers
     public class PostController : ControllerBase
     {
         private readonly IPostRepository _postRepository;
-        public PostController(IPostRepository postRepository)
+        private readonly IMapper _mapper;
+        public PostController(IPostRepository postRepository, IMapper mapper)
         {
             _postRepository = postRepository;
+            _mapper = mapper;
         }
         [HttpGet]
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            var postsDto = posts.Select(x =>
-                new PostDto
-                {
-                    PostId = x.PostId,
-                    Date = x.Date,
-                    Description = x.Description,
-                    ImageUrl = x.ImageUrl,
-                    UserId = x.UserId
-                });
+            var postsDto = _mapper.Map<IEnumerable<PostDto>>(posts);
             return Ok(postsDto);
         }
 
@@ -36,28 +31,14 @@ namespace SocialMedia.Api.Controllers
         public async Task<IActionResult> GetPostById(int postId)
         {
             var post = await _postRepository.GetPostById(postId);
-            var postDto = new PostDto
-            {
-                PostId = post.PostId,
-                Date = post.Date,
-                Description = post.Description,
-                ImageUrl = post.ImageUrl,
-                UserId = post.UserId
-            };
+            var postDto = _mapper.Map<PostDto>(post);
             return Ok(postDto);
         }
         [HttpPost]
         public async Task<IActionResult> InsertPost(PostDto _object)
         {
-            var newPost = new Post
-            {
-                Date = _object.Date,
-                Description = _object.Description,
-                ImageUrl = _object.ImageUrl,
-                UserId = _object.UserId
-            };
-
-             await _postRepository.InsertPost(newPost);
+            var newPost = _mapper.Map<Post>(_object);
+            await _postRepository.InsertPost(newPost);
             return Ok(newPost);
         }
     }
--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -9,6 +10,7 @@ using SocialMedia.Infrastrucuture.Data;
 using SocialMedia.Infrastrucuture.Data.Configuration;
 using SocialMedia.Infrastrucuture.Data.Configuration.Abstract;
 using SocialMedia.Infrastrucuture.Repositories;
+using System;
 
 namespace SocialMedia.Api
 {
@@ -24,6 +26,9 @@ namespace SocialMedia.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // configure AutoMapper to find automapper profiles
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             // configure json reference loop
             services.AddControllers()
                 .AddNewtonsoftJson(x => {
